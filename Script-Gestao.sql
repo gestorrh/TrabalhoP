a874f9ef4b525235b8fa7a2944302784cd21bfba
Lucas Rodrigues <lucasrchaves1@gmail.com>
2019-12-03 17:05:00 -0300

    dados adicionais

--- Script-Gestao.sql
+++ Script-Gestao.sql
@@ -2,13 +2,13 @@
 
 insert into usuario( id, cargo, cidade, cpf, data_nascimento, email, endereco, estado_civil, nome,
  pais, senha, setor, sexo, telefone, uf) values
-	(1, 'Gestor','Quixadá', '86355694072', '1995-10-05	',
+	(1, 'Gestor de saúde','Quixadá', '86355694072', '1995-10-05	',
 	'email1@gmail.com', 'Centro', 'SOLTEIRO', 'JOÃO',
 	'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'RH', 'MASCULINO',
-	'99887766', 'CE'),
-	(2,	'Gerente de projetos','Quixada','07114616350','1998-09-04',
+	'88996888706', 'CE'),
+	(2,	'Gestor de saúde','Quixadá','07114616350','1998-09-04',
 	'emailluccas@gmail.com', 'Francisco Eneas de Lima','SOLTEIRO', 'LUCAS',
-	'Brasil' ,'$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'TI', 'MASCULINO',
+	'Brasil' ,'$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'RH', 'MASCULINO',
 	'88996888706', 'CE');
 
 
@@ -20,7 +20,7 @@ insert into usuario( id, cargo, cidade, cpf, data_nascimento, email, endereco, e
  	(3, 'Clinico Geral','Quixadá', '91660988071', '1990-06-30',
 	'email2@gmail.com', 'Centro', 'Solteiro',
 	'PEDRO', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'SAUDE', 'MASCULINO',
-	'99887766', 'CE');	
+	'88996888706', 'CE');	
 	
 ---		COLABORADORES
 
@@ -29,12 +29,87 @@ insert into usuario( id, cargo, cidade, cpf, data_nascimento, email, endereco, e
  	(4, 'Operador','Limoeiro', '05438078009', '1993-02-07',
 	'email4@gmail.com', 'Gran Ville', 'CASADO',
 	'ALGUSTO', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'MASCULINO',
-	'99887766', 'CE'),
+	'88996888706', 'CE'),
 	
 	(5, 'Operadora','Limoeiro', '15287873077', '1995-05-05',
+	'email5@gmail.com', 'Gran Ville', 'CASADA',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(6, 'Operadora','Limoeiro', '41359162011', '1995-05-15',
+	'email6@gmail.com', 'Gran Ville', 'CASADA',
+	'JOANA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(7, 'Operadora','Limoeiro', '78941576008', '1995-05-25',
+	'email5@gmail.com', 'Gran Ville', 'CASADA',
+	'BRUNA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(8, 'Vendedora','Quixadá', '38390429098', '1995-06-06',
+	'email5@gmail.com', 'CENTRO', 'CASADA',
+	'BEATRIZ', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'VENDAS', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(9, 'Vendedora','Limoeiro', '24441671040', '1995-06-16',
+	'email5@gmail.com', 'Gran Ville', 'SOLTEIRA',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'VENDAS', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(10, 'Atendente','Fortaleza', '15404790059', '1995-06-26',
+	'email5@gmail.com', 'Messejana', 'SOLTEIRA',
+	'ANA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'SUPORTE', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(11, 'Contadora I','Limoeiro', '22514562015', '1996-03-06',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'JÚLIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'FINANCEIRO', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(12, 'Operadora','Limoeiro', '90284087033', '1996-03-16',
 	'email5@gmail.com', 'Gran Ville', 'CASADO',
 	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
-	'99887766', 'CE');	
+	'88996888706', 'CE'),
+	
+	(13, 'Operadora','Limoeiro', '32615302000', '1996-03-26',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(14, 'Operadora','Limoeiro', '91831555034', '1996-06-06',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(15, 'Operadora','Limoeiro', '71726528006', '1996-06-16',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+
+	(16, 'Operadora','Limoeiro', '59129792029', '1996-06-26',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(17, 'Operadora','Limoeiro', '56928615001', '1997-07-07',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(18, 'Operadora','Limoeiro', '80866092005', '1995-05-05',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(19, 'Operadora','Limoeiro', '06193552049', '1997-07-27',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE'),
+	
+	(20, 'Operadora','Limoeiro', '99272047013', '1997-12-12',
+	'email5@gmail.com', 'Gran Ville', 'CASADO',
+	'MARIA', 'Brasil', '$2a$10$8pXDUzKBFiPAW83kQtLDCeq3LsN8uGmAf01YLATvKUIuGEg1O.rH.', 'MONTAGEM', 'FEMININO',
+	'88996888706', 'CE');	
 	
 	
 ---		PAPEIS
@@ -53,6 +128,18 @@ insert into exame( id, avaliacao_medica, cid, crm_medico, data_exame, descricao,
 		(2, 'Muito boa', '10000', '11122233', '2019-07-01', 'Descricao aqui', '2019-12-01', 0,
 		'PERIODICO', 'ANDAMENTO', 4, 3),
 		(3, 'tensa', '10000', '11122233', '2019-01-01', 'Quebrou o braço', '2020-02-01', 2,
-		'ACIDENTE', 'ANDAMENTO', 4, 3),
+		'ACIDENTE DE TRABALHO', 'ANDAMENTO', 4, 3),
 		(4, 'Muito boa', '10000', '11122233', '2019-01-01', '', '2020-07-01', 0,
-		'DEMISSIONAL', 'ANDAMENTO', 4, 3);
+		'DEMISSIONAL', 'ANDAMENTO', 4, 3),
+		
+		(5, 'Muito boa', '10000', '11122233', '2019-01-01', 'Descricao aqui', '2019-01-01', 0,
+		'ADMISSIONAL', 'ANDAMENTO', 5, 3),
+		(6, 'Muito boa', '10000', '11122233', '2019-07-01', 'Descricao aqui', '2019-12-01', 0,
+		'PERIODICO', 'ANDAMENTO', 5, 3),
+	
+		
+		
+		;
+	
+	
+	
